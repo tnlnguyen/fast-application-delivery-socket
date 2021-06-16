cd0001b2b87a1782a90ea1dd7ef641123031edd2
Nate <nhanle1862@gmail.com>
2021-06-17 00:01:52 +0700

    Init changes

--- index.js
+++ index.js
@@ -14,63 +14,34 @@ server.listen(port, () => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Chatroom
-
-let numUsers = 0;
-
 io.on('connection', (socket) => {
-  let addedUser = false;
-
   // when the client emits 'new message', this listens and executes
   socket.on('new message', (data) => {
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
+      username: data['username'],
+      message: data['message']
     });
   });
 
   // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
+  socket.on('typing', (data) => {
     socket.broadcast.emit('typing', {
-      username: socket.username
+      username: data['username']
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
+  socket.on('stop typing', (data) => {
     socket.broadcast.emit('stop typing', {
-      username: socket.username
+      username: data['username']
     });
   });
 
   // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
+  socket.on('disconnect', (data) => {
+    socket.broadcast.emit('user left', {
+      username: data['username'],
+    });
   });
 });
