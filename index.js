c92a4c3c12762e91419d465d3fa1159735db39c6
Nate <nhanle1862@gmail.com>
2021-06-17 01:15:51 +0700

    Change again

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const server = require('http').Server(app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000));
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
+
+io = socketIO(server, {transports: ['websocket']});
 
 // Chatroom
 io.on('connection', (socket) => {
