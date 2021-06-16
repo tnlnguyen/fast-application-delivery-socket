c8ef1d65e48fa32668c862118c59eb33c159184c
Nate <nhanle1862@gmail.com>
2021-06-17 01:43:22 +0700

    Fixed again again

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ io.on('connection', function(socket){
   });
 });
 
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);	     // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
 server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
